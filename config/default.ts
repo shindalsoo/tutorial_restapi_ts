3727ff6e542cdd459107786227f3109481efe029
shindalsoo <shindalsoo@naver.com>
2024-06-05 11:38:28 +0900

    완성된 소스 업로드

---
+++ config/default.ts
@@ -0,0 +1,11 @@
+export default {
+  port: 1337,
+  dbUri: "mongodb://shindalsoo:sds9050@localhost:37017/rest-api-tutorial?authSource=admin",
+  saltWorkFactor: 10,
+  accessTokenTtl: "15m",
+  refreshTokenTtl: "1y",
+  accessTokenPrivateKey: ``,
+  accessTokenPublicKey: ``,
+  refreshTokenPrivateKey: ``,
+  refreshTokenPublicKey: ``,
+};
